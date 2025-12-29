d5ccbf5254ab4c034c7b864b61354c36b65ad229
master-sauce <idanhb17@gmail.com>
2025-12-29 23:21:46 +0200

    Add files via upload

---
+++ analyzer.ps1
@@ -0,0 +1,24 @@
+﻿Clear-Host
+while($true){
+
+$PIDs = (Get-Process -Name "$($args[0])" -ErrorAction SilentlyContinue).Id
+if ($PIDs) {
+    
+    Write-Host
+    Write-Host ("-" * 80) -ForegroundColor Gray
+    Write-Host
+    
+    # Create a regex pattern to match lines ending in a Brave PID OR indented lines
+    $pattern = " ($($PIDs -join '|'))$|^^\s{4,}"
+    NETSTAT.EXE -anob | Select-String -Pattern $pattern
+    if ($null -eq $args[1]) {
+        Start-Sleep 2
+    }
+
+    else { 
+    Start-Sleep $($args[1])
+    }
+
+}
+
+}
